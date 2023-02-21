39702c6750844204cb4bf3292285298023a00717
cspencer <cspencernd@gmail.com>
2023-02-21 12:43:45 -0500

    close menu on route change

--- src/components/common/Navbar/Hamburger.tsx
+++ src/components/common/Navbar/Hamburger.tsx
@@ -6,11 +6,17 @@ import { Transition } from "@headlessui/react"
 import { Spiral as HamburgerIcon } from "hamburger-react"
 
 /** utils */
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
 import cn from "clsx"
 
 export function Hamburger() {
     const [isShowing, setIsShowing] = useState<boolean>(false)
+
+    const pathname = usePathname()
+    useEffect(() => {
+        setIsShowing(false)
+    }, [pathname])
 
     return (
         <>
@@ -19,7 +25,13 @@ export function Hamburger() {
                 className="btn-ghost btn-square btn z-50"
                 onClick={() => setIsShowing((isShowing) => !isShowing)}
             >
-                <HamburgerIcon size={24} distance="lg" rounded label="Toggle Menu" />
+                <HamburgerIcon
+                    toggled={isShowing}
+                    size={24}
+                    distance="lg"
+                    rounded
+                    label="Toggle Menu"
+                />
             </button>
 
             <Transition show={isShowing}>
@@ -45,10 +57,13 @@ export function Hamburger() {
                     </ul>
                 </Transition.Child>
                 <Transition.Child
+                    as="label"
+                    onClick={() => setIsShowing(false)}
                     className={cn(
                         "absolute top-0 right-0",
                         "h-screen w-[20vw]",
-                        "bg-blur-300"
+                        "bg-blur-300 opacity-80",
+                        "hover:cursor-pointer hover:opacity-40 transition-all"
                     )}
                     enter="transition ease-out duration-300 transform"
                     enterFrom="translate-x-full"
@@ -72,37 +87,37 @@ const links: NavLinkData[] = [
     {
         title: "Collections Directory",
         href: "collections",
-        delay: "delay-[100ms]"
+        delay: "delay-[100ms]",
     },
     {
         title: "Staff Picks",
         href: "collections/staff-picks",
-        delay: "delay-[180ms]"
+        delay: "delay-[180ms]",
     },
     {
         title: "Summer '22",
         href: "collections/summer-22",
-        delay: "delay-[260ms]"
+        delay: "delay-[260ms]",
     },
     {
         title: "Mindset",
         href: "collections/mindset",
-        delay: "delay-[340ms]"
+        delay: "delay-[340ms]",
     },
     {
         title: "Creativity",
         href: "collections/creativity",
-        delay: "delay-[420ms]"
+        delay: "delay-[420ms]",
     },
     {
         title: "Lifestyle",
         href: "collections/lifestyle",
-        delay: "delay-[500ms]"
+        delay: "delay-[500ms]",
     },
     {
         title: "Full Catalog",
         href: "collections/catalog",
-        delay: "delay-[580ms]"
+        delay: "delay-[580ms]",
     },
 ]
 
