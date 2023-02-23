d370e35eba1508475ded18712b9b13fe42ab22cb
cspencer <cspencernd@gmail.com>
2023-02-23 06:54:01 -0500

    make menu close on link click; create delay programatically.

--- src/components/common/Navbar/Hamburger.tsx
+++ src/components/common/Navbar/Hamburger.tsx
@@ -12,10 +12,11 @@ import cn from "clsx"
 
 export function Hamburger() {
     const [isShowing, setIsShowing] = useState<boolean>(false)
+    let delay = 100
 
     const pathname = usePathname()
     useEffect(() => {
-        setIsShowing(false)
+        isShowing === true && setIsShowing(false)
     }, [pathname])
 
     return (
@@ -53,7 +54,34 @@ export function Hamburger() {
                         tabIndex={0}
                         className={cn("menu self-center", "w-[80vw] p-4")}
                     >
-                        {NavLinks}
+                        {links.map(({ title, href }, i) => {
+                            const delayClass: string = `delay-[${delay}ms]`
+                            delay += 80
+
+                            return (
+                                <Transition.Child
+                                    as="li"
+                                    key={i}
+                                    enter={cn(
+                                        "transition ease-in duration-200 transform",
+                                        delayClass
+                                    )}
+                                    enterFrom="-translate-x-[100vw]"
+                                    enterTo="-translate-x-0"
+                                    leave="transition ease-in duration-200 transform"
+                                    leaveFrom="-translate-x-0"
+                                    leaveTo="-translate-x-[100vw]"
+                                >
+                                    <Link
+                                        className="font-bold"
+                                        href={href}
+                                        onClick={() => setIsShowing(false)}
+                                    >
+                                        {title}
+                                    </Link>
+                                </Transition.Child>
+                            )
+                        })}
                     </ul>
                 </Transition.Child>
                 <Transition.Child
@@ -63,7 +91,7 @@ export function Hamburger() {
                         "absolute top-0 right-0",
                         "h-screen w-[20vw]",
                         "bg-blur-300 opacity-80",
-                        "hover:cursor-pointer hover:opacity-40 transition-all"
+                        "transition-all hover:cursor-pointer hover:opacity-40"
                     )}
                     enter="transition ease-out duration-300 transform"
                     enterFrom="translate-x-full"
@@ -80,62 +108,35 @@ export function Hamburger() {
 type NavLinkData = {
     title: string
     href: string
-    delay: string
 }
 
 const links: NavLinkData[] = [
     {
         title: "Collections Directory",
         href: "collections",
-        delay: "delay-[100ms]",
     },
     {
         title: "Staff Picks",
         href: "collections/staff-picks",
-        delay: "delay-[180ms]",
     },
     {
         title: "Summer '22",
         href: "collections/summer-22",
-        delay: "delay-[260ms]",
     },
     {
         title: "Mindset",
         href: "collections/mindset",
-        delay: "delay-[340ms]",
     },
     {
         title: "Creativity",
         href: "collections/creativity",
-        delay: "delay-[420ms]",
     },
     {
         title: "Lifestyle",
         href: "collections/lifestyle",
-        delay: "delay-[500ms]",
     },
     {
         title: "Full Catalog",
         href: "collections/catalog",
-        delay: "delay-[580ms]",
     },
 ]
-
-const NavLinks = links.map(({ title, href, delay }, i) => {
-    return (
-        <Transition.Child
-            as="li"
-            key={i}
-            enter={cn("transition ease-in duration-200 transform", delay)}
-            enterFrom="-translate-x-[100vw]"
-            enterTo="-translate-x-0"
-            leave="transition ease-in duration-200 transform"
-            leaveFrom="-translate-x-0"
-            leaveTo="-translate-x-[100vw]"
-        >
-            <Link className="font-bold" href={href}>
-                {title}
-            </Link>
-        </Transition.Child>
-    )
-})
