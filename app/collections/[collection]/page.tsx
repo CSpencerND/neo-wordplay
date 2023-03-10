d057c4f664047d01d3d05be81451efacdd79bb25
cspencer <cspencernd@gmail.com>
2023-03-09 23:22:49 -0500

    try to fix error by moving function to bottom

--- app/collections/[collection]/page.tsx
+++ app/collections/[collection]/page.tsx
@@ -2,6 +2,19 @@ import Image, { StaticImageData } from "next/image"
 import Products from "./components/Products"
 import { productsQuery } from "@/lib/productsQuery"
 
+export default async function Collections({ params }: { params: { collection: string } }) {
+    const { collection } = params
+    const { collectionTitle, productEdges } = await productsQuery(collection)
+    console.log(productEdges)
+
+    return (
+        <section className="mx-auto max-w-2xl space-y-12">
+            <h1 className="text-center text-xl font-bold text-primary">{collectionTitle}</h1>
+            <Products products={productEdges} />
+        </section>
+    )
+}
+
 export async function generateCollectionParams() {
     const collectionHandles = [
         "staff-picks",
@@ -20,17 +33,4 @@ export async function generateCollectionParams() {
         }
     })
     return collectionParams
-}
-
-export default async function Collections({ params }: { params: { collection: string } }) {
-    const { collection } = params
-    const { collectionTitle, productEdges } = await productsQuery(collection)
-    console.log(productEdges)
-
-    return (
-        <section className="mx-auto max-w-2xl space-y-12">
-            <h1 className="text-center text-xl font-bold text-primary">{collectionTitle}</h1>
-            <Products products={productEdges} />
-        </section>
-    )
 }
