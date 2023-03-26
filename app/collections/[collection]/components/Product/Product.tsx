2016ba2befa0d881a4364ac7699ff2c72985080c
cspencer <cspencernd@gmail.com>
2023-03-26 17:56:25 -0400

    refactor for SizeSelect

--- app/collections/[collection]/components/Product/Product.tsx
+++ app/collections/[collection]/components/Product/Product.tsx
@@ -3,36 +3,49 @@
 import type { ProductProps } from "@/lib/ProductStore"
 import { ShopifyProductProvider } from "@/lib/providers"
 import ProductProvider from "@/lib/providers/ProductProvider"
-import { useProduct as useShopifyProduct } from "@shopify/hydrogen-react"
-import type { Product as ProductType } from "@shopify/hydrogen-react/storefront-api-types"
+import type { ProductOption } from "@shopify/hydrogen-react/storefront-api-types"
+import ProductModal from "../Modal/ProductModal"
 import ProductLabel from "../ProductLabel"
 import Swatch from "../Swatch"
 
 function ProductWrapper({ ...initProviderProps }: ProductProps) {
     const product = initProviderProps.product
     return (
-        <ShopifyProductProvider data={product!}>
+        <ShopifyProductProvider data={product}>
             <ProductInner {...initProviderProps} />
         </ShopifyProductProvider>
     )
 }
 
 function ProductInner({ ...initProviderProps }: ProductProps) {
-    const colorOptions = useShopifyProduct().options!.find(
-        (option) => option!.name === "Color"
-    )!.values as string[]
+    const { options, id } = initProviderProps.product as {
+        options: ProductOption[]
+        id: string
+    }
+
+    const colorOptions = options.find((option) => option.name === "Color")!.values
+    const sizeOptions = options.find((option) => option.name === "Size")!.values
 
     const selectedColor = colorOptions[0]
+    const selectedSize = sizeOptions[0]
 
-    const product = initProviderProps.product as ProductType
     return (
-        <ProductProvider {...{ ...initProviderProps, colorOptions, selectedColor }}>
+        <ProductProvider
+            {...{
+                ...initProviderProps,
+                colorOptions,
+                selectedColor,
+                sizeOptions,
+                selectedSize,
+            }}
+        >
             <li
                 className="bg-blur-100 card h-full overflow-hidden rounded-2xl"
-                key={product.id}
+                key={id}
             >
-                <ProductLabel title={product.title} />
-                <Swatch />
+                <ProductLabel />
+                <Swatch className="overflow-x-auto p-3" />
+                <ProductModal />
             </li>
         </ProductProvider>
     )
