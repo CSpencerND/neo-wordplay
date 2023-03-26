3bc75e0cdbaa43d73a00d6d221cfc65b2df0ba4a
cspencer <cspencernd@gmail.com>
2023-03-26 17:52:54 -0400

    add utils directory

---
+++ app/lib/utils/index.ts
@@ -0,0 +1,1 @@
+export {sizeText} from "./sizeOptionsText"
---
+++ app/lib/utils/sizeOptionsText.ts
@@ -0,0 +1,11 @@
+export const sizeText: { [size: string]: string } = {
+    XS: "XS",
+    S: "S",
+    M: "M",
+    L: "L",
+    XL: "XL",
+    "2XL": "2X",
+    "3XL": "3X",
+    "4XL": "4X",
+    "5XL": "5X",
+}
