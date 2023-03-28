08d2d4ca488dca8fb7208033cc255514ae942241
cspencer <cspencernd@gmail.com>
2023-03-28 00:37:44 -0400

    add selected variant and add to cart

--- app/collections/[collection]/components/Modal/ProductModal.tsx
+++ app/collections/[collection]/components/Modal/ProductModal.tsx
@@ -2,7 +2,8 @@
 
 import CloseButton from "@/components/CloseButton"
 import { Dialog } from "@headlessui/react"
-import { ProductPrice } from "@shopify/hydrogen-react"
+import { AddToCartButton, ProductPrice } from "@shopify/hydrogen-react"
+import { Plus } from "react-iconly"
 import Modal from "./_Modal"
 
 import useProduct from "@/lib/hooks/useProduct"
@@ -17,7 +18,6 @@ export default function ProductModal() {
     const data = useProduct((s) => s.product)
 
     const sanitizedDescription = useSanitizedDescription()
-    const selectedOptions = useSelectedOptions()
 
     const router = useRouter()
     const pathname = usePathname()
@@ -27,6 +27,8 @@ export default function ProductModal() {
         }
     }
 
+    const { variantName, variantID } = useSelectedOptions()
+
     return (
         <Modal.Wrapper>
             <div
@@ -35,7 +37,7 @@ export default function ProductModal() {
             >
                 <CloseButton />
             </div>
-            <section>
+            <section className="h-fit overflow-hidden rounded-2xl">
                 <Modal.Image />
             </section>
             <section className="card-body space-y-3 p-0 text-sm font-bold lg:text-sm">
@@ -52,6 +54,20 @@ export default function ProductModal() {
                 </div>
                 <Modal.Swatch className="ml-0.5 [&_#swatchColor]:h-7 [&_#swatchColor]:w-7" />
                 <Modal.Size />
+
+                <span className="text-xs text-info">Selected: {variantName}</span>
+
+                <AddToCartButton
+                    variantId={variantID}
+                    accessibleAddingToCartLabel="Item is being added to cart with selected color and size"
+                    className="btn-secondary btn-block btn-sm btn flex gap-2 text-xs shadow-box"
+                >
+                    <Plus set="light" />
+                    Add to Bag
+                </AddToCartButton>
+
+                {/* TODO: <BuyNowButton /> */}
+
                 <Dialog.Description
                     as="article"
                     className="prose [&_strong]:text-accent-content"
@@ -78,7 +94,19 @@ function useSanitizedDescription() {
     return sanitizedDescription
 }
 
-function useSelectedOptions() {
+type VariantData = {
+    variantName: string | undefined
+    variantID: string | undefined
+}
+
+function useSelectedOptions(): VariantData {
+    const { variants } = useShopifyProduct()
+    if (!variants) return {} as VariantData
+    const [data, setData] = useState<VariantData>({
+        variantName: variants[0]!.selectedOptions![0]!.name as string,
+        variantID: variants[0]!.id as string,
+    })
+
     const { setSelectedOption, selectedOptions } = useShopifyProduct()
 
     const selectedColor = useProduct((s) => s.selectedColor)
@@ -93,6 +121,27 @@ function useSelectedOptions() {
         setSelectedOption("Size", selectedSize)
     }, [selectedSize])
 
-    return selectedOptions
-}
+    useUpdateEffect(() => {
+        if (!selectedOptions) return
 
+        const optionsString = JSON.stringify(
+            Object.entries(selectedOptions).map(([key, value]) => {
+                return { name: key, value: value }
+            })
+        )
+
+        variants?.forEach((variant) => {
+            if (!variant) return
+            const variantString = JSON.stringify(variant?.selectedOptions)
+            if (optionsString === variantString) {
+                const data = {
+                    variantName: variant.title,
+                    variantID: variant.id,
+                } as VariantData
+                setData(data)
+            }
+        })
+    }, [selectedOptions])
+
+    return data
+}
