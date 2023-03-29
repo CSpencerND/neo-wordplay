c82451dbcc2f5b7027715f4e419d31c4ad005ccd
cspencer <cspencernd@gmail.com>
2023-03-29 17:27:01 -0400

    ignore this file

--- app/lib/queries/getVariants.ts
+++ app/lib/queries/getVariants.ts
@@ -1,33 +1,34 @@
-import storefrontQuery from "./storefront"
-import type { SelectedOptionInput, Product } from "@shopify/hydrogen-react/storefront-api-types"
-import { SelectedOptions } from "@shopify/hydrogen-react/dist/types/ProductProvider"
+export {}
+// import storefrontQuery from "./storefront"
+// import type { SelectedOptionInput, Product } from "@shopify/hydrogen-react/storefront-api-types"
+// import { SelectedOptions } from "@shopify/hydrogen-react/dist/types/ProductProvider"
 
-export async function getVariantBySelectedOptions(
-    handle: string,
-    selectedOptions: SelectedOptions
-): Promise<Awaited<Product>> {
-    const selectedOptionInput: SelectedOptionInput[] = Object.entries(selectedOptions).map(
-        ([name, value]) => {
-            return { name, value }
-        }
-    )
+// export async function getVariantBySelectedOptions(
+//     handle: string,
+//     selectedOptions: SelectedOptions
+// ): Promise<Awaited<Product>> {
+//     const selectedOptionInput: SelectedOptionInput[] = Object.entries(selectedOptions).map(
+//         ([name, value]) => {
+//             return { name, value }
+//         }
+//     )
 
-    const vars = { productHandle: handle, selectedOptions: selectedOptionInput }
-    const data = await storefrontQuery(query, vars)
+//     const vars = { productHandle: handle, selectedOptions: selectedOptionInput }
+//     const data = await storefrontQuery(query, vars)
 
-    return data.product
-}
+//     return data.product
+// }
 
-const gql = String.raw
-const query = gql`
-    query getVariantIdBySelectedOptions(
-        $productHandle: String!
-        $selectedOptions: [SelectedOptionInput!]!
-    ) {
-        product(handle: $productHandle) {
-            variantBySelectedOptions(selectedOptions: $selectedOptions) {
-                id
-            }
-        }
-    }
-`
+// const gql = String.raw
+// const query = gql`
+//     query getVariantIdBySelectedOptions(
+//         $productHandle: String!
+//         $selectedOptions: [SelectedOptionInput!]!
+//     ) {
+//         product(handle: $productHandle) {
+//             variantBySelectedOptions(selectedOptions: $selectedOptions) {
+//                 id
+//             }
+//         }
+//     }
+// `
--- app/lib/queries/index.ts
+++ app/lib/queries/index.ts
@@ -1,4 +1,4 @@
 export { default, storefront } from "./storefront"
 export { getProductsByCollection, getFeaturedCollectionProducts } from "./getProducts"
 export { getCollections } from "./getCollections"
-export { getVariantBySelectedOptions } from "./getVariants"
+// export { getVariantBySelectedOptions } from "./getVariants"
