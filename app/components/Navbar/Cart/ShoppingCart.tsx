015a6f1710cfb879f45863256b28c8d1eff6be40
cspencer <cspencernd@gmail.com>
2023-03-30 21:38:17 -0400

    remove unused

--- app/components/Navbar/Cart/ShoppingCart.tsx
+++ app/components/Navbar/Cart/ShoppingCart.tsx
@@ -2,19 +2,7 @@
 
 import Cart from "./_Cart"
 
-// import { useUpdateEffect } from "@react-hookz/web"
-// import { useCart } from "@shopify/hydrogen-react"
-
 export default function ShoppingCart() {
-    // const { status, error } = useCart()
-    // useUpdateEffect(() => {
-    //     if (error) {
-    //         alert(error)
-    //     }
-    //     
-    //     console.log("Cart status changed", status)
-    // }, [status, error])
-
     return (
         <>
             <Cart.Button />
