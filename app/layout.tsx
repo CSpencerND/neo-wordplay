907b947811588efa501fad68468843c428e7be70
cspencer <cspencernd@gmail.com>
2023-03-30 21:21:36 -0400

    fix hrefs

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({ children }: Children) {
     const linkData: NavLinkData[] = collections.map((c, i) => {
         return {
             title: c.title,
-            href: c.handle,
+            href: `/collections/${c.handle}`,
             delay: `${baseDelay + i * 50}ms`,
         }
     })
