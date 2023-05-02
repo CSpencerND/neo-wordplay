dd77d285e97dda5553c26594bd2fa82c1f64635c
cspencer <cspencernd@gmail.com>
2023-05-02 07:12:28 -0400

    feat: add text for statement

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,21 @@ export default async function HomePage() {
                 {/*@ts-expect-error Async Component*/}
                 <Featured collectionHandle="staff-picks" />
             </Suspense>
-            <Statement heading="What we do">
-                <p>Work hard, play hard, I guess.</p>
-                <p>That is what The kids say, yeah?</p>
+            <Statement heading="Why we love what we do">
+                <p>
+                    When we think about WORDPLAY4LYFE what comes to mind is Artistic expression
+                    which is being able to express yourself freely and not be judged. We use our
+                    Imagination because the fact that you can come up with different ideas for
+                    something that's yours is just amazing. The fact that you can daydream about
+                    those different ideas and bring them to life is really exciting, which is an
+                    excellent way to share my Creativeness with the rest of the world.We want
+                    WORDPLAY4LYFE to represent Culture in all walks of life because without
+                    culture there is no WORDPLAY4LYFE.
+                </p>
+                <p>
+                    Artistic Expression, Imagination, Creativeness and Culture represents our
+                    brand. (AEICC)
+                </p>
             </Statement>
             <Gallery galleryImageData={galleryImageData} />
         </>
