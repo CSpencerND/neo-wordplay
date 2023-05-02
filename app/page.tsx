78a7dc1978fc7429b3e585c69d6f43ce55233482
cspencer <cspencernd@gmail.com>
2023-05-02 06:19:05 -0400

    perf: add suspense boundary to featured section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import { Featured, Gallery, Hero, Statement } from "@/components/home"
-import { hero, galleryImageData } from "@/static"
+import { Suspense } from "react"
+
+import { galleryImageData, hero } from "@/static"
 import font from "next/font/local"
 
 const heroFont = font({
-    src: "./static/font/don_graffiti/DonGraffiti.otf"
+    src: "./static/font/don_graffiti/DonGraffiti.otf",
 })
 
 export default async function HomePage() {
     return (
         <>
             {/*@ts-expect-error Async Component*/}
-            <Hero image={hero} font={heroFont} rounded />
-            {/*@ts-expect-error Async Component*/}
-            <Featured collectionHandle="staff-picks" />
+            <Hero
+                image={hero}
+                font={heroFont}
+                rounded
+            />
+            <Suspense>
+                {/*@ts-expect-error Async Component*/}
+                <Featured collectionHandle="staff-picks" />
+            </Suspense>
             <Statement heading="What we do">
                 <p>Work hard, play hard, I guess.</p>
                 <p>That is what The kids say, yeah?</p>
